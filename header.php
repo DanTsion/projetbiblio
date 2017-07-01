2b82aadff135df47175af44902187ed378be9229
Tonyxmxm <brame.anthony@hotmail.fr>
2017-07-01 12:42:44 +0200

    modif header

--- header.php
+++ header.php
@@ -4,14 +4,14 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-     <link rel="stylesheet" href="/projetbiblio/css/style.css">
+     <link rel="stylesheet" href="/projetbiblio-master/css/style.css">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-     <script src="/projetbiblio/js/form.js"></script>
-     <script src="/projetbiblio/js/filterTable/jquery.filtertable.js"></script>
+     <script src="/projetbiblio-master/js/form.js"></script>
+     <script src="/projetbiblio-master/js/filterTable/jquery.filtertable.js"></script>
      
      <div class="row" style="background-color:#202020">
-         <span class="col-lg-3"></span><img class="col-lg-6" src="/projetbiblio/images/logo_biblio.png" height="420" />
+         <span class="col-lg-3"></span><img class="col-lg-6" src="/projetbiblio-master/images/logo_biblio.png" height="420" />
      </div>
      
      <nav class="navbar navbar-inverse">
@@ -20,25 +20,25 @@
                 <span class= "active navbar-brand" >Bibliotheque</span>
             </div> 
         <ul class="nav navbar-nav">
-            <li><a href="/projetbiblio/index.php">Accueil</a></li>
+            <li><a href="/projetbiblio-master/index.php">Accueil</a></li>
             <li class="dropdown">
                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">Client
                 <span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="/projetbiblio/html/creationClient.php">Creation client</a></li>
-                    <li><a href="/projetbiblio/html/gestionClient.php">Gestion client</a></li>
+                    <li><a href="/projetbiblio-master/html/creationClient.php">Creation client</a></li>
+                    <li><a href="/projetbiblio-master/html/gestionClient.php">Gestion client</a></li>
                 </ul>
             </li>
            <li class="dropdown">
                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">Livre
                 <span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="/projetbiblio/html/creationLivre.php">Creation livre</a></li>
-                    <li><a href="/projetbiblio/html/gestionLivre.php">Gestion livre</a></li>
+                    <li><a href="/projetbiblio-master/html/creationLivre.php">Creation livre</a></li>
+                    <li><a href="/projetbiblio-master/html/gestionLivre.php">Gestion livre</a></li>
                 </ul>
             </li>    
                <li>
-                <a href="/projetbiblio/html/creationEmprunt.php">Emprunt</a>
+                <a href="/projetbiblio-master/html/creationEmprunt.php">Emprunt</a>
                </li> 
         </ul>
         </div>        
